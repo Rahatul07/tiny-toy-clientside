42be305155df466ca13db2eb8d40692f47766d97
Rahatul07 <rifarhad08@gmail.com>
2023-05-20 12:44:56 +0600

    homepage improved

--- src/pages/ToyCategory/ToyCategory.jsx
+++ src/pages/ToyCategory/ToyCategory.jsx
@@ -1,8 +1,20 @@
-import React from "react";
-import category1 from "../../../src/assets/single-toy-images/avengers/HOT TOYS Hulk Sixth Scale Figure - The Avengers.png";
-import category2 from "../../../src/assets/single-toy-images/transformers/Buy Transformers Studio Series 105 Mirage Deluxe ROTB Movie Toy – Collecticon Toys.png";
-import category3 from "../../../src/assets/single-toy-images/star wars/Hasbro Star Wars - Toys, Action Figures, Characters & Adventure Figure Collection.png";
+import React, { useEffect, useState } from "react";
+import category1 from "../../../src/assets/single-toy-images/avengers/category-avenger.png";
+import category2 from "../../../src/assets/single-toy-images/transformers/category-transformer.png";
+import category3 from "../../../src/assets/single-toy-images/star wars/category-star-wars.png";
 const ToyCategory = () => {
+  const [toys, setToys] = useState([]);
+  const [activeTab, setActiveTab] = useState("Star Wars");
+  useEffect(() => {
+    fetch(`https://tiny-toy-server.vercel.app/toys/${activeTab}`)
+      .then((res) => res.json())
+      .then((result) => {
+        setToys(result);
+      });
+  }, [activeTab]);
+  const handleTabClick = (tabName) => {
+    setActiveTab(tabName);
+  };
   return (
     <div>
       <div className=" p-5">
@@ -10,38 +22,59 @@ const ToyCategory = () => {
           Category
         </h1>
         <div className=" w-10/12  mx-auto my-10  md:flex justify-between">
-          <div className="card h-56 flex-col items-center relative mb-5 w-40 rounded-xl bg-white border-4 border-primary hover:border-warning">
+          <div
+            onClick={() => handleTabClick("Avengers")}
+            className={`card h-56 bg-primary text-white flex-col items-center relative mb-5 w-40 rounded-xl  text-2xl  border-4  border-primary Avenger ${
+              activeTab == "Avengers"
+                ? " border-warning bg-warning  text-gray-800"
+                : ""
+            }`}
+          >
             <img
               className="h-40 w-full btn bg-white border-none hover:bg-white "
               src={category1}
               alt=""
             />
 
-            <h1 className="btn  w-40  hover:bg-warning border-none hover:text-black absolute bottom-0 rounded-none btn-primary m-2 ">
+            <button className="font-bold mx-auto rounded-none  m-2 ">
               Avengers
-            </h1>
+            </button>
           </div>
-          <div className="card h-56 flex-col items-center relative mb-5 w-40 rounded-xl bg-white border-4 border-primary hover:border-warning">
+          <div
+            onClick={() => handleTabClick("Transformers")}
+            className={`card h-56 bg-primary text-white flex-col items-center relative mb-5 w-40 rounded-xl  text-2xl  border-4  border-primary Avenger ${
+              activeTab == "Transformers"
+                ? " border-warning bg-warning  text-gray-800"
+                : ""
+            }`}
+          >
             <img
               className="h-40 w-full btn bg-white border-none hover:bg-white "
               src={category2}
               alt=""
             />
 
-            <h1 className="btn  w-40 hover:bg-warning border-none hover:text-black  absolute bottom-0 rounded-none btn-primary m-2 ">
+            <button className="font-bold mx-auto rounded-none  m-2 ">
               Transformers
-            </h1>
+            </button>
           </div>
-          <div className="card h-56 flex-col items-center relative mb-5 w-40 rounded-xl bg-white border-4 border-primary hover:border-warning">
+          <div
+            onClick={() => handleTabClick("Star Wars")}
+            className={`card h-56 bg-primary text-white flex-col items-center relative mb-5 w-40 rounded-xl  text-2xl  border-4  border-primary Avenger ${
+              activeTab == "Star Wars"
+                ? " border-warning bg-warning  text-gray-800"
+                : ""
+            }`}
+          >
             <img
               className="h-40 w-full btn bg-white border-none hover:bg-white "
               src={category3}
               alt=""
             />
 
-            <h1 className="btn  w-40  hover:bg-warning border-none hover:text-black absolute bottom-0 rounded-none btn-primary m-2 ">
+            <button className="font-bold mx-auto rounded-none  m-2 ">
               Star Wars
-            </h1>
+            </button>
           </div>
         </div>
       </div>
