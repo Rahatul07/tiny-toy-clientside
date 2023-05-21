945f094614d694ad48764bbcfcbca312b9e0c1d0
Rahatul07 <rifarhad08@gmail.com>
2023-05-21 20:22:36 +0600

    aos applied

--- src/pages/CategoryDetails/CategoryDetails.jsx
+++ src/pages/CategoryDetails/CategoryDetails.jsx
@@ -3,6 +3,27 @@ import { Rating } from "@smastrom/react-rating";
 import "@smastrom/react-rating/style.css";
 import { BsFillArrowRightCircleFill } from "react-icons/bs";
 import { Link } from "react-router-dom";
+import AOS from "aos";
+import "aos/dist/aos.css";
+AOS.init();
+AOS.init({
+  disable: false,
+  startEvent: "DOMContentLoaded",
+  initClassName: "aos-init",
+  animatedClassName: "aos-animate",
+  useClassNames: false,
+  disableMutationObserver: false,
+  debounceDelay: 50,
+  throttleDelay: 99,
+  offset: 120,
+  delay: 0,
+  duration: 400,
+  easing: "ease",
+  once: false,
+  mirror: false,
+  anchorPlacement: "top-bottom",
+});
+AOS.refresh();
 const CategoryDetails = ({ toy }) => {
   return (
     <div
@@ -10,32 +31,51 @@ const CategoryDetails = ({ toy }) => {
       style={{
         backgroundImage: `linear-gradient(to right,   rgba(120, 0, 260, 0.8),rgba(100, 1, 254, 0.2))`,
       }}
+      data-aos="fade-up"
+      data-aos-offset="200"
+      data-aos-duration="700"
+      data-aos-easing="ease-in-out"
+      data-aos-mirror="true"
+      data-aos-once="false"
     >
-      <figure className="px-4 pt-4  h-fit">
-        <img src={toy.image} alt="Shoes" className="rounded-lg" />
-      </figure>
-      <div className="card-body pl-10 text-white">
-        <h2 className="font-bold text-warning text-lg">
-          Title:
-          <span className="font-normal ml-3 text-white">{toy.title}</span>
-        </h2>
+      <div className="group relative cursor-pointer items-center justify-center overflow-hidden transition-shadow hover:shadow-xl hover:shadow-black/30 ">
+        <div className="h-full   w-auto">
+          <img
+            className="h-fit w-full object-cover transition-transform duration-500 group-hover:rotate-3 group-hover:scale-125 px-4 pt-4 "
+            src={toy.image}
+            alt=""
+          />
+        </div>
+        <div className="absolute inset-0 bg-gradient-to-b from-transparent via-transparent to-black group-hover:from-black/70 group-hover:via-black/60 group-hover:to-black/70"></div>
+        <div className="absolute inset-0 flex translate-y-[60%] flex-col items-center justify-center px-9 text-center transition-all duration-500 group-hover:translate-y-0">
+          <div className="card-body pl-10 text-white">
+            <h2 className="font-bold text-warning text-lg">
+              Title:
+              <span className="font-normal ml-3 text-white">{toy.title}</span>
+            </h2>
 
-        <h2 className="font-bold text-lg text-warning pb-5">
-          Toy price:
-          <span className="font-normal ml-3 text-white">${toy.price}</span>
-        </h2>
+            <h2 className="font-bold text-lg text-warning pb-5">
+              Toy price:
+              <span className="font-normal ml-3 text-white">${toy.price}</span>
+            </h2>
 
-        <div className="flex justify-between items-center ">
-          <div className=" flex gap-2 items-center  absolute bottom-5 left-10">
-            <Rating style={{ maxWidth: 130 }} value={toy.ratings} readOnly />
-            {toy.ratings}
-          </div>
-          <div className="absolute bottom-3 right-5">
-            <Link to={`/singleToyDetails/${toy._id}`} title="View details">
-              <button>
-                <BsFillArrowRightCircleFill className="text-warning h-8 w-8" />
-              </button>
-            </Link>
+            <div className="flex justify-between items-center ">
+              <div className=" flex gap-2 items-center  absolute bottom-5 left-10">
+                <Rating
+                  style={{ maxWidth: 130 }}
+                  value={toy.ratings}
+                  readOnly
+                />
+                {toy.ratings}
+              </div>
+              <div className="absolute bottom-3 right-5">
+                <Link to={`/singleToyDetails/${toy._id}`} title="View details">
+                  <button>
+                    <BsFillArrowRightCircleFill className="text-warning h-8 w-8" />
+                  </button>
+                </Link>
+              </div>
+            </div>
           </div>
         </div>
       </div>
--- src/pages/Gallery/Gallery.jsx
+++ src/pages/Gallery/Gallery.jsx
@@ -8,10 +8,40 @@ import img6 from "../../assets/single-toy-images/transformers/Transformers Toys 
 import img7 from "../../assets/single-toy-images/star wars/Star Wars Action Figures.png";
 import img8 from "../../assets/single-toy-images/transformers/Transformers Toys Studio Series 74 Deluxe Class Transformers.png";
 import LazyLoad from "react-lazy-load";
+import AOS from "aos";
+import "aos/dist/aos.css";
+AOS.init();
+AOS.init({
+  disable: false,
+  startEvent: "DOMContentLoaded",
+  initClassName: "aos-init",
+  animatedClassName: "aos-animate",
+  useClassNames: false,
+  disableMutationObserver: false,
+  debounceDelay: 50,
+  throttleDelay: 99,
+  offset: 120,
+  delay: 0,
+  duration: 400,
+  easing: "ease",
+  once: false,
+  mirror: false,
+  anchorPlacement: "top-bottom",
+});
+AOS.refresh();
+
 const Gallery = () => {
   return (
     <LazyLoad>
-      <div className="my-24">
+      <div
+        className="my-24"
+        data-aos="fade-up"
+        data-aos-offset="200"
+        data-aos-duration="700"
+        data-aos-easing="ease-in-out"
+        data-aos-mirror="true"
+        data-aos-once="false"
+      >
         <div className="text-center">
           <h1 className="text-5xl mb-5 font-bold text-white">Core features</h1>
           <p className="mb-10 w-8/12 mx-auto text-white">
@@ -25,77 +55,93 @@ const Gallery = () => {
           </p>
         </div>
         <div className=" mx-10 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-5">
-          <div className="card w-auto bg-warning shadow-xl mx-2">
-            <figure className=" p-2 ">
+          <div className="group relative cursor-pointer items-center justify-center overflow-hidden transition-shadow hover:shadow-xl hover:shadow-black/30 border-8 border-primary">
+            <div className="h-full   w-auto">
               <img
-                src={img}
-                alt="toy"
-                className="rounded-xl object-contain w-full h-full"
-              />
-            </figure>
-          </div>
-          <div className="card w-auto bg-warning shadow-xl mx-2">
-            <figure className=" p-2 ">
-              <img
-                src={img2}
-                alt="toy"
-                className="rounded-xl object-contain w-full h-full"
-              />
-            </figure>
-          </div>
-          <div className="card w-auto bg-warning shadow-xl mx-2">
-            <figure className=" p-2 ">
-              <img
-                src={img3}
-                alt="toy"
-                className="rounded-xl object-contain w-full h-full"
-              />
-            </figure>
-          </div>
-          <div className="card w-auto bg-warning shadow-xl mx-2">
-            <figure className=" p-2 ">
-              <img
-                src={img4}
-                alt="toy"
-                className="rounded-xl object-contain w-full h-full"
-              />
-            </figure>
-          </div>
-          <div className="card w-auto bg-warning shadow-xl mx-2">
-            <figure className=" p-2 ">
-              <img
-                src={img5}
-                alt="toy"
-                className="rounded-xl object-contain w-full h-full"
-              />
-            </figure>
-          </div>
-          <div className="card w-auto bg-warning shadow-xl mx-2">
-            <figure className=" p-2 ">
-              <img
-                src={img6}
-                alt="toy"
-                className="rounded-xl object-contain w-full h-full"
-              />
-            </figure>
-          </div>
-          <div className="card w-auto bg-warning shadow-xl mx-2">
-            <figure className=" p-2 ">
-              <img
-                src={img7}
-                alt="toy"
-                className="rounded-xl object-contain w-full h-full"
-              />
-            </figure>
-          </div>
-          <div className="card w-auto bg-warning shadow-xl mx-2">
-            <figure className=" p-2 ">
-              <img
+                className="h-full w-full object-cover transition-transform duration-500 group-hover:rotate-3 group-hover:scale-125"
                 src={img8}
-                alt="toy"
-                className="rounded-xl object-contain w-full h-full"
+                alt=""
               />
-            </figure>
+            </div>
+            <div className="absolute inset-0 bg-gradient-to-b from-transparent via-transparent to-primary group-hover:from-black/70 group-hover:via-black/60 group-hover:to-black/70"></div>
+            <div className="absolute inset-0 flex translate-y-[60%] flex-col items-center justify-center px-9 text-center transition-all duration-500 group-hover:translate-y-0"></div>
+          </div>
+          <div className="group relative cursor-pointer items-center justify-center overflow-hidden transition-shadow hover:shadow-xl hover:shadow-black/30 border-8 border-primary">
+            <div className="h-full   w-auto">
+              <img
+                className="h-full w-full object-cover transition-transform duration-500 group-hover:rotate-3 group-hover:scale-125"
+                src={img2}
+                alt=""
+              />
+            </div>
+            <div className="absolute inset-0 bg-gradient-to-b from-transparent via-transparent to-primary group-hover:from-black/70 group-hover:via-black/60 group-hover:to-black/70"></div>
+            <div className="absolute inset-0 flex translate-y-[60%] flex-col items-center justify-center px-9 text-center transition-all duration-500 group-hover:translate-y-0"></div>
+          </div>
+          <div className="group relative cursor-pointer items-center justify-center overflow-hidden transition-shadow hover:shadow-xl hover:shadow-black/30 border-8 border-primary">
+            <div className="h-full   w-auto">
+              <img
+                className="h-full w-full object-cover transition-transform duration-500 group-hover:rotate-3 group-hover:scale-125"
+                src={img3}
+                alt=""
+              />
+            </div>
+            <div className="absolute inset-0 bg-gradient-to-b from-transparent via-transparent to-primary group-hover:from-black/70 group-hover:via-black/60 group-hover:to-black/70"></div>
+            <div className="absolute inset-0 flex translate-y-[60%] flex-col items-center justify-center px-9 text-center transition-all duration-500 group-hover:translate-y-0"></div>
+          </div>
+          <div className="group relative cursor-pointer items-center justify-center overflow-hidden transition-shadow hover:shadow-xl hover:shadow-black/30 border-8 border-primary">
+            <div className="h-full   w-auto">
+              <img
+                className="h-full w-full object-cover transition-transform duration-500 group-hover:rotate-3 group-hover:scale-125"
+                src={img4}
+                alt=""
+              />
+            </div>
+            <div className="absolute inset-0 bg-gradient-to-b from-transparent via-transparent to-primary group-hover:from-black/70 group-hover:via-black/60 group-hover:to-black/70"></div>
+            <div className="absolute inset-0 flex translate-y-[60%] flex-col items-center justify-center px-9 text-center transition-all duration-500 group-hover:translate-y-0"></div>
+          </div>
+          <div className="group relative cursor-pointer items-center justify-center overflow-hidden transition-shadow hover:shadow-xl hover:shadow-black/30 border-8 border-primary">
+            <div className="h-full   w-auto">
+              <img
+                className="h-full w-full object-cover transition-transform duration-500 group-hover:rotate-3 group-hover:scale-125"
+                src={img5}
+                alt=""
+              />
+            </div>
+            <div className="absolute inset-0 bg-gradient-to-b from-transparent via-transparent to-primary group-hover:from-black/70 group-hover:via-black/60 group-hover:to-black/70"></div>
+            <div className="absolute inset-0 flex translate-y-[60%] flex-col items-center justify-center px-9 text-center transition-all duration-500 group-hover:translate-y-0"></div>
+          </div>
+          <div className="group relative cursor-pointer items-center justify-center overflow-hidden transition-shadow hover:shadow-xl hover:shadow-black/30 border-8 border-primary">
+            <div className="h-full   w-auto">
+              <img
+                className="h-full w-full object-cover transition-transform duration-500 group-hover:rotate-3 group-hover:scale-125"
+                src={img6}
+                alt=""
+              />
+            </div>
+            <div className="absolute inset-0 bg-gradient-to-b from-transparent via-transparent to-primary group-hover:from-black/70 group-hover:via-black/60 group-hover:to-black/70"></div>
+            <div className="absolute inset-0 flex translate-y-[60%] flex-col items-center justify-center px-9 text-center transition-all duration-500 group-hover:translate-y-0"></div>
+          </div>
+          <div className="group relative cursor-pointer items-center justify-center overflow-hidden transition-shadow hover:shadow-xl hover:shadow-black/30 border-8 border-primary">
+            <div className="h-full   w-auto">
+              <img
+                className="h-full w-full object-cover transition-transform duration-500 group-hover:rotate-3 group-hover:scale-125"
+                src={img7}
+                alt=""
+              />
+            </div>
+            <div className="absolute inset-0 bg-gradient-to-b from-transparent via-transparent to-primary group-hover:from-black/70 group-hover:via-black/60 group-hover:to-black/70"></div>
+            <div className="absolute inset-0 flex translate-y-[60%] flex-col items-center justify-center px-9 text-center transition-all duration-500 group-hover:translate-y-0"></div>
+          </div>
+          <div className="group relative cursor-pointer items-center justify-center overflow-hidden transition-shadow hover:shadow-xl hover:shadow-black/30 border-8 border-primary">
+            <div className="h-full   w-auto">
+              <img
+                className="h-full w-full object-cover transition-transform duration-500 group-hover:rotate-3 group-hover:scale-125"
+                src={img}
+                alt=""
+              />
+            </div>
+            <div className="absolute inset-0 bg-gradient-to-b from-transparent via-transparent to-primary group-hover:from-black/70 group-hover:via-black/60 group-hover:to-black/70"></div>
+            <div className="absolute inset-0 flex translate-y-[60%] flex-col items-center justify-center px-9 text-center transition-all duration-500 group-hover:translate-y-0"></div>
           </div>
         </div>
       </div>
